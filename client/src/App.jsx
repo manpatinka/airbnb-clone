bacac4a93ed19381145f9738d97d83ea098135f7
Evgenia <evgeniayurkova@gmail.com>
2024-05-24 09:26:53 +0300

    final commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,6 @@ import PlacesPage from './pages/PlacesPage';
 import PlacesFormPage from './pages/PlacesFormPage';
 import PlacePage from './pages/PlacePage';
 import BookingsPage from './pages/BookingsPage';
-import BookingPage from './pages/BookingPage';
 
 axios.defaults.baseURL = 'http://localhost:4000';
 axios.defaults.withCredentials = true;
